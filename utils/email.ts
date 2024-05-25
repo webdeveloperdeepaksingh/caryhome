9a4c68be3f52e635cada035d9838425d8cde241c
Deepak Singh <webdeveloperdeepaksingh@gmail.com>
2024-05-25 22:23:18 +0530

    hu

--- utils/email.ts
+++ utils/email.ts
@@ -1,15 +1,24 @@
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 
+interface TransportOptions {
+    host: string;
+    port: number;
+    auth: {
+      user: string;
+      pass: string;
+    };
+  }
+  
 const sendEmail = async (option:any) => {
 //Using transporter service to send email.
     const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
+        host: process.env.EMAIL_HOST as string,
+        port: process.env.EMAIL_PORT as unknown as number,
         auth:{
-            user:process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASSWORD
+            user:process.env.EMAIL_USER as string,
+            pass: process.env.EMAIL_PASSWORD as string
         }
-    })
+    } as TransportOptions)
 //Defining email options
     const emailOptions = {
         from: 'support@cineflix.com',
